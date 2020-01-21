09dfc93a10b6e75dd573a52eefd9adc5aa7c2547
Brad <brad@viziworx.com>
2020-01-21 13:25:35 -0500

    handling transitions

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 function createMachine(stateMachineDefinition) {
 
     const machine = {
+        // machine object
+        // add the value and transition properties
+        // currentState would be something like 'off' or 'on' in this case and event would be 'switch' for the toggle
+        value: stateMachineDefinition.initialState,
+        transition(currentState, event) {
+            // The event is checked against the current state's transitions
+            const currentStateDefinition = stateMachineDefinition[currentState]
+            const destinationTransition = currentStateDefinition.transitions[event]
+            // If a transition matches the event, that transition "happens"
+            // If the user defined a transition from the current state with this event, then it'll continue, otherwise, it'll exit early
+            if (!destinationTransition) {
+                return
+            }
+            const destinationState = destinationTransition.target
+            const destinationStateDefinition = stateMachineDefinition[destinationState]
 
+            // By virtur of a transition "happening", states are exited, and entered and the relevant actions are performed
+            // Call the action for the transition, the onExit for the current state and the onEnter for the next state.
+            destinationTransition.action()
+            currentStateDefinition.actions.onExit()
+            destinationStateDefinition.actions.onEnter()
+
+            // The machine immediately is in the new state, ready to process the next event.
+            machine.value = destinationState
+
+            return machine.value
+        },
 }
-
     return machine
-
 }
 
 // create the state machine
